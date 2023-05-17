0a3f9afb59c3c217940ee79054f1597775d006ac
Luis <luisperi18@hotmail.com>
2023-05-18 00:19:58 +0200

    Add pet model

---
+++ src/models/pets.ts
@@ -0,0 +1,25 @@
+export type PetStructure = {
+  id: string;
+  name: string;
+  breed: string;
+  isAdopted: boolean;
+  owner: string;
+};
+
+export class Pets implements PetStructure {
+  static generateID() {
+    return Math.trunc(Math.random() * 100).toString();
+  }
+
+  id: string;
+  breed: string;
+  isAdopted: boolean;
+
+  constructor(public name: string, breed: string, public owner: string) {
+    this.id = Pet.generateID();
+    this.name = name;
+    this.breed = breed;
+    this.isAdopted = false;
+    this.owner = owner;
+  }
+}
