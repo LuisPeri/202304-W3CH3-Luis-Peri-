5ca83d111ec041eae1dc793d1f94da8e82635f88
Luis <luisperi18@hotmail.com>
2023-05-18 00:11:56 +0200

    Add type files

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+import { Footer } from "../footer";
+import { Header } from "../header";
+import { Main } from "../main";
+import { Pet } from "../pet";
+
+new Header("#app", "Lista de mascotas");
+new Main("#app");
+new Pet("main");
+new Footer("#app", "Cansino´s");
