4bada0ff668b2acdbf5e35755b8e0af954ce07a3
Luis <luisperi18@hotmail.com>
2023-05-18 00:05:03 +0200

    Add mock-pet

---
+++ src/data/mock.pet.ts
@@ -0,0 +1,7 @@
+import { Pet } from "../pet";
+
+export const getMockPets = (): Pet[] => [
+  new Pet("Sardinilla", "Gato Camavinga", "Chupagladiolos"),
+  new Pet("Aeris", "Gato Lechoso", "Nasus"),
+  new Pet("Lux", "Europoeo", "She Peri"),
+];
